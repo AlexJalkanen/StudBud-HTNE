1322a59b0fb505a34076275a5b6f18cf2b7c5075
Alex Jalkanen <alexjalkanen@gmail.com>
2020-06-07 03:10:50 -0400

    getSBR Table

---
+++ backend/Lambdas/getAllSBR/index.js
@@ -0,0 +1,36 @@
+'use strict'
+const AWS = require('aws-sdk');
+
+AWS.config.update({region: "us-east-2"});
+
+exports.handler = async (event, context) => {
+    const documentClient = new AWS.DynamoDB.DocumentClient({region: "us-east-2"});
+
+    let reponseBody = ""
+    let statusCode = 0;
+
+    const params = {
+        TableName: "StudBudRequests"
+    };
+
+    try {
+        const data = await documentClient.scan(params).promise();
+        reponseBody = JSON.stringify(data);
+        statusCode = 200;
+    }
+    catch (err) {
+        reponseBody = `Unable to get SBR table: ${err}`
+        statusCode = 500;
+    }
+
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json",
+            "access-control-allow-origin": "*"
+        },
+        body: reponseBody
+    };
+
+    return response;
+}
--- frontend/src/components/SBRs.vue
+++ frontend/src/components/SBRs.vue
@@ -1,112 +1,53 @@
 <template>
     <v-data-table
       :headers="headers"
-      :items="desserts"
+      :items="items"
       :items-per-page="10"
       class="elevation-1"
     ></v-data-table>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'SBRs',
   data () {
     return {
       headers: [
         {
-          text: 'Dessert (100g serving)',
+          text: 'Host',
           align: 'start',
           sortable: false,
-          value: 'name',
+          value: 'host',
         },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' },
+        { text: 'Subject', value: 'subject' },
+        { text: 'Virtual', value: 'virtual' },
+        { text: 'Start Time', value: 'starttime' },
+        { text: 'End Time', value: 'endtime' },
+        { text: 'Max StudBuds', value: 'maxPeople' },
+        { text: 'School', value: 'school' },
+        { text: 'Location', value: 'location' },
+        //     created: created,
+        //     starttime: starttime,
+        //     endtime: endtime,
+        //     open: open,
+        //     maxPeople: maxPeople,
+        //     location: location,
+        //     school: school,
+        //     subject: subject,
+        //     virtual: virtual, 
+        //     studbuds: studbuds
       ],
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%',
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%',
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%',
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%',
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%',
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%',
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%',
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%',
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%',
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%',
-        },
-      ],
+      items: []
+    }
+  },
+  async mounted () {
+    try {
+        const response = await axios.get('https://8wugvfc99e.execute-api.us-east-2.amazonaws.com/prod/sbr');
+        this.items = response.data["Items"];
+    }
+    catch (error) {
+        console.log(error)
     }
   },
 }
